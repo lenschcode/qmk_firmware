92cb3fb51f0a95ec38a13a74a20a8130073741a6
lenschcode <danlennardhaensch@gmail.com>
2023-08-07 22:44:26 +0200

    updated macos compos

--- keyboards/bastardkb/charybdis/3x5/keymaps/chourrage/keycodes.h
+++ keyboards/bastardkb/charybdis/3x5/keymaps/chourrage/keycodes.h
@@ -12,6 +12,10 @@ enum charybdis_keymap_layers {
 #define ESC_LAY LT(_FUNC, KC_ESC)
 #define OS_SHFT OSM(MOD_LSFT)
 
+#define MOD_LCMD MOD_LGUI
+#define MOD_LOPT MOD_LALT
+#define MOD_ROPT MOD_RALT
+
 #define KC_AE RALT(KC_Q)//RALT(KC_A) -- for eurkey
 #define KC_OE RALT(KC_P)//RALT(KC_O)
 #define KC_UE RALT(KC_Y)//RALT(KC_U)
